81d0c0372417879dd4db487ad887560e4fca3b20
ossthun <msline3@bluewin.ch>
2026-05-14 17:32:33 +0200

    Update contact information in privacy section

--- pages/privacy.js
+++ pages/privacy.js
@@ -169,8 +169,7 @@ export default function PrivacyPage() {
           <h2 style={styles.heading}>16. Contact</h2>
           <p style={styles.text}>
             If you have privacy-related questions, requests, or concerns, please
-            contact the website operator using the contact information provided
-            on the website.
+            contact the website operator using this address: fintool-kit@)proton.me
           </p>
         </section>
 
