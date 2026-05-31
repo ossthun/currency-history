678b4bac8132a3b3d3e1c161c7ceedb2799f53c7
ossthun <msline3@bluewin.ch>
2026-05-31 19:36:07 +0200

    Add Google AdSense and favicon to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,16 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Google AdSense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5958056334367225"
           crossOrigin="anonymous"
         />
+
+        {/* Favicon */}
+        <link rel="icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/icon.png" />
       </Head>
 
       <Component {...pageProps} />
